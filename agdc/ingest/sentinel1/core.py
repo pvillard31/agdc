309a0d2e5562ccabd0957607782a2c96bba12699
David Bastide <bastidav@yahoo.fr>
2015-11-19 18:41:48 +0100

    Sentinel ingestor progress. Access to the ingested data has to be tested.

---
+++ agdc/ingest/sentinel1/core.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+# ===============================================================================
+# Copyright 2015 Geoscience Australia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============================================================================
+
+"""
+Ingester for Sentinel datasets.
+"""
+from __future__ import absolute_import
+
+import os
+import logging
+from os.path import basename
+
+from osgeo import gdal
+
+from eotools.execute import execute
+
+from agdc.ingest import SourceFileIngester
+from agdc.cube_util import DatasetError
+from .sentinel_dataset import SentinelDataset
+
+_LOG = logging.getLogger(__name__)
+
+
+def _is_sentinel_file(filename):
+    """
+    Does the given file match a Modis NetCDF file?
+
+    (we could make this more extensive in the future, but it's directly derived from the old find_files() logic.
+
+    :type filename: str
+    :rtype: bool
+    >>> d = '/g/data/u39/public/data/modis/datacube/mod09-swath/terra/2010/12/31'
+    >>> f = 'MOD09_L2.2010365.2300.20130130162407.remapped_swath_500mbands_0.005deg.nc'
+    >>> _is_modis_file(f)
+    True
+    >>> _is_modis_file(os.path.join(d, f))
+    True
+    >>> _is_modis_file(d)
+    False
+    """
+    basename = os.path.basename(filename).lower()
+    #basename.startswith('sentinel') and
+    return filename.endswith(".img")
+
+
+class SentinelIngester(SourceFileIngester):
+    """Ingester class for Sentinel datasets."""
+
+    def __init__(self, datacube=None, collection=None):
+        super(SentinelIngester, self).__init__(_is_sentinel_file, datacube, collection)
+
+    def open_dataset(self, dataset_path):
+        """Create and return a dataset object.
+
+        dataset_path: points to the dataset to be opened and have
+           its metadata read.
+        """
+
+        return SentinelDataset(dataset_path)
+
+    def filter_dataset(self, path, row, date):
+        """Return True if the dataset should be included, False otherwise.
+
+        Overridden to allow NULLS for row
+        """
+        (start_date, end_date) = self.get_date_range()
+
+        # Modis 'path' is actually the Orbit number.
+        (min_path, max_path) = self.get_path_range()
+
+        include = ((max_path is None or path is None or int(path) <= int(max_path)) and
+                   (min_path is None or path is None or int(path) >= int(min_path)) and
+                   (end_date is None or date is None or date <= end_date) and
+                   (start_date is None or date is None or date >= start_date))
+
+        return include
+
+    def preprocess_dataset(self, dataset_list):
+        """Performs pre-processing on the dataset_list object.
+
+        dataset_list: list of datasets to be opened and have
+           its metadata read.
+        """
+
+
+        return dataset_list
--- tests/tests_CG/TestLandsatIngester.py
+++ tests/tests_CG/TestLandsatIngester.py
@@ -7,11 +7,68 @@ Ingester for landsat datasets.
 from __future__ import absolute_import
 
 import logging
+import gdal
+import struct
+from gdalconst import *
+
 
 from agdc.ingest.landsat.core import LandsatIngester
+from agdc.ingest.sentinel1.core import SentinelIngester
 
-_LOG = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG);
+_LOG = logging.getLogger('TestCG')
+
+if True:
+    from agdc.ingest import run_ingest
+    run_ingest(SentinelIngester)
+
+if False:
+    dataset = gdal.Open( "/data/agdc/S1_orthorect/water_after.img", GA_ReadOnly )
+    if dataset is None:
+        print 'error reading s1 dataset'
+    else:
+        print 'ok, s1 dataset reading ok'
 
 
-from agdc.ingest import run_ingest
-run_ingest(LandsatIngester)
+
+        cols = dataset.RasterXSize
+        rows = dataset.RasterYSize
+        bands = dataset.RasterCount
+        driver = dataset.GetDriver().LongName
+
+        geotransform = dataset.GetGeoTransform()
+        print cols , ' columns'
+        print rows, ' rows'
+        print bands, ' bands'
+        print driver, ' driver'
+        print  geotransform
+
+        originX = geotransform[0]
+        originY = geotransform[3]
+        pixelWidth = geotransform[1]
+        pixelHeight = geotransform[5]
+        print 'origin X', originX
+        print 'originY', originY
+        print 'pixelWidth', pixelWidth
+        print 'pixelHeight',pixelHeight
+
+
+        band = dataset.GetRasterBand(1)
+        print 'Band Type=',gdal.GetDataTypeName(band.DataType)
+        min = band.GetMinimum()
+        max = band.GetMaximum()
+        if min is None or max is None:
+            (min,max) = band.ComputeRasterMinMax(1)
+        print 'Min=%.3f, Max=%.3f' % (min,max)
+        if band.GetOverviewCount() > 0:
+            print 'Band has ', band.GetOverviewCount(), ' overviews.'
+        if not band.GetRasterColorTable() is None:
+            print 'Band has a color table with ', band.GetRasterColorTable().GetCount(), ' entries.'
+
+        # TODO range
+        for i in range(0, 1):
+                # def ReadRaster(self, xoff, yoff, xsize, ysize, buf_xsize = None, buf_ysize = None, buf_type = None, band_list = None )
+            scanline = band.ReadRaster( 0, i, band.XSize, 1, band.XSize, 1, GDT_Float32 )
+            tuple_of_floats = struct.unpack('f' * band.XSize, scanline)
+            print 'Line ', i, ' has ', len(tuple_of_floats), " values."
+
